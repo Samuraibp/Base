f95836a01492e4784c844cb3b54d72071bdcd489
Andrey <andreykushnir2021@gmail.com>
2026-01-21 19:38:24 +0200

    Updated

--- Base/main.cpp
+++ Base/main.cpp
@@ -7,14 +7,38 @@ public:
     static int vehiclesCount;
     static int petrolcount;
     static int goodsCount;
+    void PlusPetrol(int p)
+    {
+        petrolcount += p;
+    }
 };
 
 class Vehicel
 {
-    const int driver = 1;
+    int driver = 1;
     int petrol;
 public:
+    Vehicel(): driver(0), petrol(0) {}
+    Vehicel(int p)
+    {
+        petrol = p;
+    }
+    void SetPet(int p)
+    {
+        petrol = p;
+    }
+    int GetPetr()
+    {
+        return petrol;
+    }
+    int GetDriver()
+    {
+        return driver;
+    }
     static int MaxPetrol;
+    virtual void arrive() {}
+    virtual void leave() {}
+
 };
 
 class Bas : protected Vehicel
@@ -22,6 +46,20 @@ class Bas : protected Vehicel
     int passengers;
 public:
     static int MaxPassengers;
+    void arrive () override {
+        Base::vehiclesCount++;
+        Base::peopleCount += passengers + GetDriver();
+        Base::petrolcount += GetPetr();
+
+        cout << "Bus arrived\n";
+    }
+    void leave() override {
+        Base::vehiclesCount--;
+        Base::peopleCount -= passengers + GetDriver();
+        Base::petrolcount -= GetPetr();
+
+        cout << "Bus left\n";
+    }
 };
 
 class Truck : protected Vehicel
@@ -29,6 +67,22 @@ class Truck : protected Vehicel
     int goods;
 public:
     static int MaxGoods;
+    void arrive() override {
+        Base::vehiclesCount++;
+        Base::peopleCount += GetDriver();
+        Base::petrolcount += GetPetr();
+        Base::goodsCount += goods;
+
+        cout << "Truck arrived\n";
+    }
+    void leave() override {
+        Base::vehiclesCount--;
+        Base::peopleCount -= GetDriver();
+        Base::petrolcount -= GetPetr();
+        Base::goodsCount -= goods;
+
+        cout << "Truck left\n";
+    }
 };
 
 
@@ -39,4 +93,4 @@ int Base::goodsCount = 0;
 
 int Vehicel::MaxPetrol = 100;
 int Bas::MaxPassengers = 50;
-int Truck::MaxGoods = 1000;
+int Truck::MaxGoods = 100;
